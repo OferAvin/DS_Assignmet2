d4fc371882c241afff5129ab71422c4df6b6a8b9
Ofer Avin <oferavin32@gmail.com>
2020-04-11 19:38:38 +0300

    added delete and insert to backtrackingArray

--- src/BacktrackingArray.java
+++ src/BacktrackingArray.java
@@ -1,12 +1,13 @@
 public class BacktrackingArray implements Array<Integer>, Backtrack {
     private Stack stack;
     private int[] arr;
-    // TODO: implement your code here
+    private int size;
 
     // Do not change the constructor's signature
     public BacktrackingArray(Stack stack, int size) {
         this.stack = stack;
         arr = new int[size];
+        size = 0; 
     }
 
     @Override
@@ -17,28 +18,36 @@ public class BacktrackingArray implements Array<Integer>, Backtrack {
     @Override
     public Integer search(int x) {
         int ans=-1;
-        for(int i=0;i<arr.length & ans==-1;i=i+1) {
+        for(int i=0; i<size; i=i+1) {
         	if(arr[i]==x) {
         		ans=i;
+        		break;
+        		
         	}
         }
         return ans;
     }
 
     @Override
+    //this method adds x at the end of the array
     public void insert(Integer x) {
-        int[] arr2=new int[arr.length+1];
-        for(int i=0;i<arr.length;i=i+1) {
-        	arr2[i]=arr[i];
-        }
-        arr2[arr.length+1]=x;
-        stack.push(arr);
-        arr=arr2;
+    	if(size == arr.length) {}	
+    	arr[size] = x;
+    	size = size + 1;
+    	stack.push(true);
+	
     }
 
     @Override
-    public void delete(Integer index) {
-        // TODO: implement your code here
+    public void delete(Integer value) {
+    	if(value >= size) {}
+    	int index = search(value);
+    	if(index != -1) {
+    		arr[index] = arr[size];
+	    	stack.push(value);
+	    	stack.push(index);
+	    	stack.push(false);
+    	}
     }
 
     @Override
@@ -75,4 +84,5 @@ public class BacktrackingArray implements Array<Integer>, Backtrack {
     public void print() {
         // TODO: implement your code here
     }
+    
 }

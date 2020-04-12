c54934e2c2263b75d677bfe6b99d6258f1c6c80c
tamir_Shilony <tamirshilony@gmail.com>
2020-04-12 13:42:57 +0300

    final BacktrackingArray

--- src/BacktrackingArray.java
+++ src/BacktrackingArray.java
@@ -21,8 +21,7 @@ public class BacktrackingArray implements Array<Integer>, Backtrack {
         for(int i=0; i<size; i=i+1) {
         	if(arr[i]==x) {
         		ans=i;
-        		break;
-        		
+        		break;      		
         	}
         }
         return ans;
@@ -119,9 +118,7 @@ public class BacktrackingArray implements Array<Integer>, Backtrack {
         		arr[index] = value;
         		size = size+1;
         	}
-        }
-        
-        	
+        }        	
     }
 
     @Override
@@ -136,32 +133,5 @@ public class BacktrackingArray implements Array<Integer>, Backtrack {
        System.out.print(arr[size-1]);
        System.out.println();
     }
-
-    public static void main(String[] args) {
-    	BacktrackingArray tamirBacktrackingArray=new BacktrackingArray(new Stack(), 9);
-    	tamirBacktrackingArray.insert(3);
-    	tamirBacktrackingArray.insert(9);
-    	tamirBacktrackingArray.print();
-    	tamirBacktrackingArray.backtrack();
-    	tamirBacktrackingArray.print();
-    	tamirBacktrackingArray.insert(1);
-    	tamirBacktrackingArray.insert(4);
-    	tamirBacktrackingArray.insert(7);
-    	tamirBacktrackingArray.print();
-    	System.out.println(tamirBacktrackingArray.maximum());
-    	System.out.println(tamirBacktrackingArray.minimum());
-    	System.out.println(tamirBacktrackingArray.predecessor(1));
-    	tamirBacktrackingArray.delete(9);
-    	tamirBacktrackingArray.print();
-    	tamirBacktrackingArray.delete(1);
-    	tamirBacktrackingArray.print();
-    	tamirBacktrackingArray.backtrack();
-    	tamirBacktrackingArray.print();
-    	tamirBacktrackingArray.backtrack();
-    	tamirBacktrackingArray.print();
-    	System.out.println(tamirBacktrackingArray.maximum());
-    	System.out.println(tamirBacktrackingArray.minimum());
-    	System.out.println(tamirBacktrackingArray.predecessor(1));
-	}
 }
 
